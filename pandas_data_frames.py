7072fab3f9ec75c8814017b988f6319f3f2b49a4
D.P <dhanyata2512@gmail.com>
2025-10-07 18:58:28 +0100

    Update pandas_data_frames.py

--- pandas_data_frames.py
+++ pandas_data_frames.py
@@ -13,3 +13,38 @@ print(df2)
 
 #fetch complete info
 df.info()
+
+#descriptive statistics 
+print(df.describe())
+
+# read csv file
+titanic_df=pd.read_csv(r"C:\Users\Bhulku\Desktop\jetlearn\Data Science DS\titanic.csv")
+titanic_df.info()
+print(titanic_df)
+
+#fetch records from top
+print(titanic_df.head(15))
+
+#fetch records from bottom
+print(titanic_df.tail())
+
+#No.rows and columns
+print(titanic_df.shape)
+
+#retrive single column
+print(titanic_df["Name"])
+
+print(titanic_df["Age"].mean())
+print(titanic_df["Fare"].sum())
+
+#retrive multiple column
+print(titanic_df[["Name","Age"]])
+
+#filtering
+print(titanic_df[titanic_df["Age"]<18])
+
+print(titanic_df[(titanic_df["Age"]<18)&(titanic_df["Pclass"]==1)])# & --> and  ----- use | ---> or
+
+#slicing with index
+print(titanic_df.iloc[10:20:2,2:5])
+print(titanic_df.iloc[[13,14,5,7,34,67,92],[2,3,5]])
