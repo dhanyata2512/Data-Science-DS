5f73af4664d42700eed8104aff577b1aed9094dd
D.P <dhanyata2512@gmail.com>
2025-10-28 18:57:59 +0000

    Update pandas_hw.py

--- pandas_hw.py
+++ pandas_hw.py
@@ -1,18 +1,27 @@
 import pandas as pd
 
-    
-names = []
-scores = []
+student_data = {}
 
-def student_data():
-    for i in range(5):
-        names.append(input(f"Enter name for student {i+1}: "))
-        scores.append(int(input(f"Enter score for student {i+1}: ")))
-
-student_data()
-print("\nStudent Scores")
+for i in range(5):
+    name = input(f"Enter name of student {i+1}: ")
+    score = float(input(f"Enter score for {name}: "))
+    student_data[name] = score
 
 
-series=pd.Series(scores,index=names)
-print(series)
+scores= pd.Series(student_data)
 
+average_score = scores.mean()
+max_score = scores.max()
+min_score = scores.min()
+total_score = scores.sum()
+
+print(f"Average Score: {average_score}")
+print(f"Maximum Score: {max_score}")
+print(f"Minimum Score: {min_score}")
+print(f"Total Score: {total_score}")
+
+ascending = scores.sort_values()
+descending = scores.sort_values(ascending=False)
+
+print(ascending)
+print(descending)
