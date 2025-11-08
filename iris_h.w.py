5cc1dcc8b9b51cd71386b9a67ff1884e5f53254a
D.P <dhanyata2512@gmail.com>
2025-11-08 08:52:02 +0000

    Update iris_h.w.py

--- iris_h.w.py
+++ iris_h.w.py
@@ -1,24 +1,29 @@
 import pandas as pd
 
+pd.set_option('future.no_silent_downcasting', True)
+
 iris_df=pd.read_csv(r"C:\Users\Bhulku\Desktop\jetlearn\Data Science DS\iris.csv")
 
 print(iris_df.head(15))
-
-print(iris_df["petal_length"].mean())
-print(iris_df["petal_width"].mean())
-print(iris_df["sepal_length"].mean())
-print(iris_df["sepal_width"].mean())
-
-print(iris_df["petal_length"].min())
-print(iris_df["petal_width"].min())
-print(iris_df["sepal_length"].min())
-print(iris_df["sepal_width"].min())
-
-print(iris_df["petal_length"].max())
-print(iris_df["petal_width"].max())
-print(iris_df["sepal_length"].max())
-print(iris_df["sepal_width"].max())
+print(iris_df.shape)
+print(iris_df.info())
+print(iris_df.tail(8))
 
 print(iris_df["species"].value_counts())
 
+print(iris_df[iris_df["species"]=="setosa"])
 
+print(iris_df[iris_df["petal_length"]>iris_df["petal_length"].mean()])
+
+print(iris_df.agg({"petal_length":["mean","min","max"],"petal_width":["mean","min","max"],"sepal_width":["mean","min","max"],"sepal_length":["mean","min","max"]}))
+
+iris_df["specimen"]=iris_df["species"].replace({"setosa":1,"virginica":2,"versicolor":3})
+print(iris_df)
+
+print(iris_df.sort_values("petal_length"))
+print(iris_df.groupby("species").mean())
+
+iris_df.rename(columns={"sepal_length":"sepal lenght","sepal_width":"sepal width","petal_width":"petal width"},inplace=True)
+print(iris_df)
+
+print(iris_df["species"].str.upper())
